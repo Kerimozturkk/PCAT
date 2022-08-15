22283236b02adcbc483e163a6f1f5020a18b5742
Kerim Öztürk <kerim_648@hotmail.com>
2022-08-15 18:00:05 +0300

    download package

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const fn = function () {
-  console.log('Func is working');
-};
+const express = require('express');
 
-fn();
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req,res) => {
+
+    const photo = {
+        id:1,
+        name:"Photo Name",
+        description:"Photo Description"
+    }
+    res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} da başlatıldı`);
+});
