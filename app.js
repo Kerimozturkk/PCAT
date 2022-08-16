bb5b487b14cbb726daa70b08f1e6ba4b7d127a5d
Kerim Öztürk <kerim_648@hotmail.com>
2022-08-16 16:18:41 +0300

    middleware

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const port = 3000;
+//MIDDLEWARES
+app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-
-    const photo = {
-        id:1,
-        name:"Photo Name",
-        description:"Photo Description"
-    }
-    res.send(photo);
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu port ${port} da başlatıldı`);
