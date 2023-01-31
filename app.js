f5b4c08c8a929fc33f53254d4617c2fc1f84c44c
Kerim Öztürk <kerim_648@hotmail.com>
2023-01-31 12:52:50 +0300

    mongodb connection and photo schema created

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const path = require('path');
+const Photo = require('./models/Photo');
+
+
 const app = express();
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -9,13 +19,16 @@ app.set('view engine', 'ejs');
 //MIDDLEWARES
 app.use(express.static('public'));
 
-app.use(express.urlencoded({extended:true})); //url'deki datayı okur
-app.use(express.json()); // url'deki datayı jsona çevirir.
-//postu yakalamamızı sağlayan core express middleware'ı normalde body parser kullanılır.
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  const photos = await Photo.find({});
+  res.render('index',{
+    photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -26,8 +39,8 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => { //add.ejs yaptığım yönlendirmeyi yakalıyorum...
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
   res.redirect('/');
 });
 
